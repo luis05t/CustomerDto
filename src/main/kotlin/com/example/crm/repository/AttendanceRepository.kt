08adce890cb577f6ec19579fc9368f614ee4cb39
luis05t <latinoco@sudamericano.edu.ec>
2024-11-05 22:44:13 -0500

    Revert "luis"
    This reverts commit 81a71711258e39755e481e7d9a831e161a6b19ec.

--- src/main/kotlin/com/example/crm/repository/AttendanceRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.crm.repository
-
-import com.example.crm.entity.Attendance
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface AttendanceRepository : JpaRepository<Attendance, Long>
--- src/main/resources/db/migration/V1__create_customer.sql
+++ src/main/resources/db/migration/V1__create_customer.sql
@@ -1,23 +1,9 @@
--- Employees Table
-CREATE TABLE employees (
-                           employee_id SERIAL PRIMARY KEY,
-                           first_name VARCHAR(100) NOT NULL,
-                           last_name VARCHAR(100) NOT NULL,
-                           position VARCHAR(100) NOT NULL,
-                           department VARCHAR(100) NOT NULL,
-                           salary DECIMAL(10, 2) NOT NULL,
-                           hiring_date DATE NOT NULL
-);
+CREATE TABLE IF NOT EXISTS customer(
+    id SERIAL,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) ,
+    address VARCHAR(255) ,
+    PRIMARY KEY(id)
 
--- Attendance Table
-CREATE TABLE attendances (
-                             id SERIAL PRIMARY KEY,
-                             employee_id INTEGER NOT NULL,
-                             date DATE NOT NULL,
-                             check_in TIME NOT NULL,
-                             check_out TIME NOT NULL,
-                             status VARCHAR(20) NOT NULL,
-                             attendance_type VARCHAR(20) NOT NULL,  -- Type of attendance (e.g. Regular, Late, Absent)
-                             FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE,
-                             CHECK (check_out > check_in)  -- Ensures check_out is later than check_in
 );
