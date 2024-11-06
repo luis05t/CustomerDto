d58fc4a184269410977681bcd6375944f26a0f1f
luis05t <latinoco@sudamericano.edu.ec>
2024-11-05 22:50:37 -0500

    Revert "Reapply "luis""
    This reverts commit b8af3fee0c2bc180542ff40d8f9921c4dd09f94e.

--- src/main/kotlin/com/example/crm/repository/CustomerRepository.kt
+++ src/main/kotlin/com/example/crm/repository/CustomerRepository.kt
@@ -1,8 +1,9 @@
 package com.example.crm.repository
 
-import com.example.crm.entity.Employee
+import com.example.crm.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+
 @Repository
-interface EmployeeRepository : JpaRepository<Employee, Long>
+interface CustomerRepository:JpaRepository<Customer, Long>
